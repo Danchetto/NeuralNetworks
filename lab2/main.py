e349a26fe4373d9368c4fee0e531518689cab09b
Danchetto <danchetto@yandex.ru>
2018-04-20 19:13:42 +0300

    update learning cycle

--- lab2/main.py
+++ lab2/main.py
@@ -1,3 +1,6 @@
+import math
+import matplotlib.pyplot as plt
+
 class Perceptron:
     learning_norm = 0.5
     p = 4
@@ -20,14 +23,17 @@ class Perceptron:
         print('Weights: ' + self.weights.__str__() + '\n')
         counter = self.p
         new_weights = []
-        while counter < 20:
+        all_result = correct_data[0:self.p]
+        while counter <= 20:
             values = []
             for cycle in range(self.p):
-                values.append(correct_data[counter - self.p - cycle + 1])
+                values.append(correct_data[counter - self.p + cycle])
+            print(values)
+            print('\n')
 
             self.set_values(values)
-            current_result = round(self.next_x(), 6)
-            print(current_result)
+            current_result = self.next_x()
+            all_result.append(current_result)
 
             new_weights = self.weights
             for i in range(len(self.weights)):
@@ -37,36 +43,48 @@ class Perceptron:
                 error_count += 1
 
             counter += 1
-        self.weights = new_weights
-        return error_count
 
-    # def get_result(self, a):
-    #     result = []
-    #     for data in table:
-    #         self.set_values(data)
-    #         result.append(self.next_x())
-    #
-    #     return result
+        self.weights = new_weights
+        return all_result
+
+
+def error_function(x, correct_x):
+    diff = []
+    for i in range(len(x)):
+        diff.append((x[i] - correct_x[i]) ** 2)
+    return math.sqrt(sum(diff))
 
 
 perceptron = Perceptron()
 
 correct_data = []
-a = -1
-b = 1
-delta = abs(a - b) / 20
-def my_func(x):
+x = []
+left = -1
+right = 1
+delta = abs(left - right) / 20
+
+
+def square(x):
     return x**2
 
+
 for i in range(1, 22):
-    correct_data.append(round(my_func(a), 6))
-    a += delta
+    correct_data.append(round(square(left), 6))
+    x.append(left)
+    left += delta
 
 perceptron.set_weights([0, 0, 0, 0, 0])
 
 steps = 0
-while perceptron.learning_cycle(correct_data) != 0:
+while steps < 90:
+    perceptron.learning_cycle(correct_data)
     steps += 1
     continue
+
+result = perceptron.learning_cycle(correct_data)
+error = round(error_function(result, correct_data), 5)
+print(result)
+print(correct_data)
+print('Error = ', error)
 
 print("DONE in {steps} steps".format(steps=steps))
